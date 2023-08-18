5882ff0a0711874590d0f80b92525b7a470ee4de
savirvadrevu <90135913+savirvadrevu@users.noreply.github.com>
2023-08-18 16:11:45 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:100
+    
+    })
+    camera = document.getElementById("cAMERA")
+    Webcam.attach(camera)
+    
